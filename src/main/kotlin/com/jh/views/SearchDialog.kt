13a8ee5eb949e9ee6f403a2624aa410c5e99b865
WandererN <izhdanovn@gmail.com>
2018-09-25 00:33:13 +0300

    SearchDialog SearchResultsView temlates

---
+++ src/main/kotlin/com/jh/views/SearchDialog.kt
@@ -0,0 +1,8 @@
+package views
+
+import javafx.scene.layout.BorderPane
+import tornadofx.*
+
+class SearchDialog : View("My View") {
+    override val root: BorderPane by fxml()
+}
---
+++ src/main/kotlin/com/jh/views/SearchResultsView.kt
@@ -0,0 +1,8 @@
+package com.jh.views
+
+import javafx.scene.layout.BorderPane
+import tornadofx.*
+
+class SearchResultsView : View("My View") {
+    override val root: BorderPane by fxml()
+}
