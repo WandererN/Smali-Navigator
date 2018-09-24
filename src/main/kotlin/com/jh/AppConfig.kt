3252d9e76c681163a69c6703749d5b532f4c52b7
WandererN <izhdanovn@gmail.com>
2018-09-25 00:34:30 +0300

    Application name moved to internalization strings

--- src/main/kotlin/com/jh/AppConfig.kt
+++ src/main/kotlin/com/jh/AppConfig.kt
@@ -2,10 +2,12 @@ package com.jh
 
 import org.json.simple.JSONObject
 import org.json.simple.parser.JSONParser
+import tornadofx.*
+import tornadofx.FX.Companion.messages
 
 object AppConfig {
     const val PROJECT_ROOT = "debugRoot"
-    const val CONST_APP_TITLE = "Smali navigator "
+    val CONST_APP_TITLE = messages["app_title"]
     var apkToolPath: String = "$PROJECT_ROOT/apktool.jar"
 
     fun inflateFromJson(jsonText: String) {
