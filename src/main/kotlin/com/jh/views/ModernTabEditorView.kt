f39d36e7f7a2a32e3ccc4b58393148b5943c96c5
WandererN <izhdanovn@gmail.com>
2018-10-03 00:03:33 +0300

    find usages initially implemented

--- src/main/kotlin/com/jh/views/ModernTabEditorView.kt
+++ src/main/kotlin/com/jh/views/ModernTabEditorView.kt
@@ -3,14 +3,17 @@ package com.jh.views
 import com.jh.Workspace
 import com.jh.smaliStructs.SmaliClass
 import com.jh.smaliStructs.SmaliMethod
+import com.jh.util.findTextAndShowDialog
 import com.jh.util.loadPicture
+import com.jh.util.logger
 import javafx.scene.control.ListView
 import javafx.scene.control.SplitPane
 import javafx.scene.image.ImageView
 import tornadofx.*
 import java.io.File
 
-class ModernTabEditorView(var currentFile: File) : View("My View") {
+class ModernTabEditorView(var currentFile: File = File("tmp.tmp")) : View("My View") {
+
     private val methodImage = loadPicture("/icons/method_icon.png")
     private val constructorImage = loadPicture("/icons/constructor_icon.png")
 
@@ -19,14 +22,28 @@ class ModernTabEditorView(var currentFile: File) : View("My View") {
     val methodsNamesListView: ListView<SmaliMethod> by fxid("methods_list")
 
     lateinit var smaliClass: SmaliClass
-
     private val methodsPopupMenu = contextmenu {
         item("Goto declaration")
-        item("Find usages")
+        item("Find usages") {
+            setOnAction {
+                val selected = methodsNamesListView.selectedItem ?: return@setOnAction
+                var combinedParams = selected.parameters.joinToString(";")
+                if (combinedParams.isNotEmpty())
+                    combinedParams+=";"
+                val combinedName = "L${smaliClass.packageName.replace(".", "/")}/${smaliClass.name};->${selected.name}($combinedParams)"
+                logger.info(combinedName)
+                findTextAndShowDialog(combinedName)
+            }
+        }
         item("Rename")
     }
 
     init {
+
+        if (!currentFile.exists()) ///REMOVE LATER
+            currentFile.createNewFile()
+
+
         val clazz = Workspace.loadedClasses.find { smaliClass -> smaliClass.file == currentFile }
         clazz?.let { smaliClass = it }
         methodsNamesListView.cellFormat { smaliMethod ->
@@ -44,13 +61,13 @@ class ModernTabEditorView(var currentFile: File) : View("My View") {
             }
         }
         methodsNamesListView.contextMenu = methodsPopupMenu
-
     }
 
     fun jumpToLine(line: Int) {
         editorWindowView.moveTo(editorWindowView.position(line, 0).toOffset())
         editorWindowView.requestFollowCaret() //TODO more correct jump. (jump to line and scroll +-some lines)
         editorWindowView.requestFocus()
+
 
     }
 
